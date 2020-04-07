07c86198945f9c24fec721ef364545f977bfa953
Christopher Scott <cmdruid@aptget.website>
2020-04-07 14:01:40 -0700

    added xkcd demo

--- patatap/static/js/patatap.js
+++ patatap/static/js/patatap.js
@@ -1,86 +1,97 @@
 // Init the global list variable for our circles.
-var circles = []
+console.log("PointText Finished.");
+
+const circles = [];
 
 // Init the filename array of our sound files.
-var sounds = ['bubbles', 'clay', 'confetti', 'corona', 'dotted-spiral',
-              'flash-1', 'flash-2', 'flash-3', 'glimmer', 'moon', 'pinwheel',
-              'piston-1', 'piston-2', 'piston-3', 'prism-1', 'prism-2',
-              'prism-3', 'splits', 'squiggle', 'strike', 'suspension', 'timer',
-              'ufo', 'veil', 'wipe', 'zig-zag'
-            ]
+const sounds  = ['bubbles', 'clay', 'confetti', 'corona', 'dotted-spiral',
+                 'flash-1', 'flash-2', 'flash-3', 'glimmer', 'moon', 'pinwheel',
+                 'piston-1', 'piston-2', 'piston-3', 'prism-1', 'prism-2',
+                 'prism-3', 'splits', 'squiggle', 'strike', 'suspension', 'timer',
+                 'ufo', 'veil', 'wipe', 'zig-zag'
+                ];
 
 // Draw some helper text on the canvas.
-var text = new PointText(new Point(view.size.width / 2, 50));
+let text         = new PointText(new Point(view.size.width / 2, 50));
+
 text.justification = 'center';
-text.fontFamily = 'Roboto';
-text.fontSize = '20px';
-text.fillColor = 'white';
-text.content = 'Press any key from a to z...';
+text.fontFamily    = 'Roboto';
+text.fontSize      = '20px';
+text.fillColor     = 'white';
+text.content       = 'Press any key from a to z...';
 
 function createCircle(size, color) {
   /* Draw a circle at a random point within the browser window,
      give it a :size: and :color:, then add it to the array. */
 
-  var maxPoint = new Point(view.size.width, view.size.height);
-  var randomPoint = Point.random();
-  var point = maxPoint * randomPoint;
-
-  var newCircle = new Path.Circle(point, size);
-  newCircle.fillColor = color;
-  circles.push(newCircle);
+	let maxPoint    = new Point(view.size.width, view.size.height),
+      	randomPoint = Point.random(),
+      	point       = maxPoint * randomPoint,
+		newCircle   = new Path.Circle(point, size);
+		  
+  	newCircle.fillColor = color;
+	circles.push(newCircle);
+	console.log("createCircle Finished.");
 }
 
 function playSound(filename) {
-  /* Fetch the sound file via :filename:, then play the sound. */
+	/* Fetch the sound file via :filename:, then play the sound. */
 
-  var sound = new Howl({src: ['static/sounds/' + filename + '.mp3']});
-  sound.play();
+	let sound = new Howl({src: ['static/sounds/' + filename + '.mp3']});
+	sound.play();
 }
 
 function random(range) {
-  /* Generate a random number within :range: values. */
+  	/* Generate a random number within :range: values. */
 
-  return Math.floor(Math.random() * (range));
+  	return Math.floor(Math.random() * (range));
 }
 
 function randomColor(key) {
-  /* Use the range of alphabet characters (0-25) in order to select an
-     rgb channel, then bias that channel using the key character. */
+  	/* Use the range of alphabet characters (0-25) in order to select an
+       rgb channel, then bias that channel using the key character. */
 
-  // Create a random rgba value (hue remains fixed).
-  rgba = [random(256),random(256),random(256),1]
-  // Floor-divide the key by 8 in order to get a number between 0-2.
-  channel = Math.floor(key / 8);
-  // Make sure the provided number does not exceed 2.
-  index = (channel < 3) ? channel : 2;
-  // Use the calcualated number to select an rgb channel, then add a bias.
-  rgba[index] = ((key % 8) * key) + 100;
-  // Return the resulting rgb value.
-  return 'rgba(' + rgba.toString() + ')';
+  	// Create a random rgba value (hue remains fixed).
+	rgba = [random(256),random(256),random(256),1];
+	  
+  	// Floor-divide the key by 8 in order to get a number between 0-2.
+	channel = Math.floor(key / 8);
+	  
+  	// Make sure the provided number does not exceed 2.
+	index = (channel < 3) ? channel : 2;
+	  
+  	// Use the calcualated number to select an rgb channel, then add a bias.
+	rgba[index] = ((key % 8) * key) + 100;
+	  
+  	// Return the resulting rgb value.
+  	return 'rgba(' + rgba.toString() + ')';
 }
 
 function onKeyDown(e) {
-  /* Main event loop. On key-press event, get the alphanumeric code of
-     the key pressed, then use it to generate a circle with a unique
-     color and sound. */
+  	/* Main event loop. On key-press event, get the alphanumeric code of
+       the key pressed, then use it to generate a circle with a unique
+       color and sound. */
 
-  keyCode = e.event.keyCode;
-  key = (96 < keyCode && keyCode < 123) ? keyCode - 97 : 0;
-  createCircle(500, randomColor(key));
-  playSound(sounds[key]);
+  	const keyCode = e.event.keyCode,
+	 	  key     = (96 < keyCode && keyCode < 123) ? keyCode - 97 : 0;
+			
+  	createCircle(500, randomColor(key));
+  	playSound(sounds[key]);
 }
 
 function onFrame(e) {
-  /* Main animation loop. For each circle, on every frame, reduce its scale
-     while also icnreasing its color hue. Once the circle shrinks below a
-     certain size, remove it from the array. */
+  	/* Main animation loop. For each circle, on every frame, reduce its scale
+       while also increasing its color hue. Once the circle shrinks below a
+       certain size, remove it from the array. */
      
-  for (var i = 0; i < circles.length; i++) {
-    circles[i].fillColor.hue += 1;
-    circles[i].scale(.9);
-    if (circles[i].area < 1) {
-      circles[i].remove();
-      circles.splice(i, 1);
-    }
-  }
+  	if (circles.length) circles.forEach((circle, i) => {
+
+    	circle.fillColor.hue += 1;
+		circle.scale(0.9);
+		
+    	if (circle.area < 1) {
+      		circle.remove();
+      		circles.splice(i, 1);
+    	}
+  	});
 }
---
+++ xkcd-demo/script.js
@@ -0,0 +1,88 @@
+console.log("script.js loaded!")
+
+const app       = document.getElementById('root'),
+      logo      = document.createElement('img'),
+	  container = document.createElement('div'),
+	  cardCount = 9;
+
+renderCards(cardCount);
+
+async function fetchXKCDComic(id) {
+	/*  */
+
+  	const comicId = id ? id : '',
+		  url     = `https://proxima-api.herokuapp.com/${comicId}`,
+		  res     = await fetch(url);
+	
+	if (!(res && res.status === 200)) {
+		console.log(url, res)
+		throw new Error("Bad response.");
+	}
+
+  	return await res.json();
+}
+
+function generateCard(comic) {
+  	/*  */
+
+	const image = comic.imgRetina || comic.img,
+		  url   = `https://xkcd.com/${comic.num}`,
+		  title = comic.safe_title,
+		  alt   = comic.alt,
+		  date  = `${comic.month}/${comic.day}/${comic.year}`;
+
+	const card    = document.createElement('a'),
+		  div     = document.createElement('div'),
+		  img     = document.createElement('img'),
+		  content = document.createElement('div'),
+		  header  = document.createElement('div'),
+		  meta    = document.createElement('div'),
+		  desc    = document.createElement('div');
+
+
+	card.setAttribute('class', 'ui card raised centered');
+	card.setAttribute('href', url);
+
+	div.setAttribute('class', 'image');
+	card.appendChild(div);
+	
+	img.setAttribute('class', 'ui image');
+	img.setAttribute('src', image);
+	div.appendChild(img);
+
+	content.setAttribute('class', 'content');
+	card.appendChild(content);
+
+	header.textContent = title
+	header.setAttribute('class', 'header');
+	content.appendChild(header);
+
+	meta.textContent = date;
+	meta.setAttribute('class', 'meta');
+	content.appendChild(meta);
+
+	desc.setAttribute('class', 'description');
+	desc.textContent = alt;
+	content.appendChild(desc);
+
+  	return card;
+}
+
+async function renderCards(count) {
+  	/* */
+
+  	try {
+		const latestComic = await fetchXKCDComic(),
+			  latestId    = latestComic.num,
+			  container   = document.querySelector('#cardDeck');
+			  
+		console.log(latestComic, latestId);
+
+      	for (let i = 0; i < count; i++) {
+          	const comic = await fetchXKCDComic(latestId - i),
+                  card = generateCard(comic);
+
+          	container.appendChild(card);
+      	}
+  	} catch(err) { console.log(err); }
+}
