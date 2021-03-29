05b755e2f5bde51a8589fde98475c45d49f10a2d
Christopher Scott <cscottdev@protonmail.com>
2021-03-28 20:12:08 -0700

    Only way to test navbar is to commit...

--- navbar.js
+++ navbar.js
@@ -1,14 +1,18 @@
 console.log("navbar.js loaded!");
 
 async function main() {
+  /* Main event loop. All declarations are wrapped within 
+     this scope. */
   const [ _, repo, path = '' ] = getURL();
-  const ghURL    = `https://github.com/cmdruid${repo}tree/master${path}`,
+  const ghURL    = `https://github.com/cmdruid/${repo}/tree/master/${path}`,
         cssURL   = 'https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.css',
         localdir = document.currentScript.src.match(/^.*\//)[0];
-  console.log(ghURL )
+  console.log('Github URL set to: ', ghURL);
   loadLink('fomantic-ui', cssURL);
   loadNav(localdir + 'navbar.html');
+
   async function loadNav(path) {
+    /* Handles loading of the navigation bar. */
     const navbar  = loadHTML(await fetchHTML(path)),
           homeBtn = navbar.querySelector('#home-link'),
           ghBtn   = navbar.querySelector('#github-link');
@@ -19,16 +23,18 @@ async function main() {
   }
 
   function getURL() {
+    /* Returns the correct URL for linking to Github. */
     let devmode = window.location.hostname.includes('127.0.0.1'),
         isRoot  = !window.location.pathname;
     switch (true) {
-      case devmode: return [ null, '/web-dev/', '' ];
+      case devmode: return [ null, 'web-dev', '' ];
       case isRoot:  return [ null, windows.hostname, '' ];
-      default: return window.location.pathname.match(/^(\/[\w\-]*\/)(.*)$/); 
+      default: return window.location.pathname.match(/^\/([\w\-]*)\/(.*)$/);
     }
   }
 
   async function fetchHTML(path) {
+    /* Fetches the locally stored HTML template. */
     try { 
       let res = await fetch(path);
       if (!res.ok) throw Error(`Failed with status: ${res.status}`); 
@@ -37,12 +43,14 @@ async function main() {
   }
 
   function loadHTML(HTMLtxt) {
+    /* Loads the locally stored HTML template as DOM. */
     let tmp = document.createElement('template');
     tmp.innerHTML = HTMLtxt.trim();
     return tmp.content.firstElementChild;
   }
 
   function loadLink(name, url) {
+    /* Checks that the required CSS styleshit is loaded. */
     let links = document.head.querySelectorAll('link');
     for (let link of links) if (link.href.includes(name)) return;
     document.head.prepend(loadHTML(`<link rel="stylesheet" type="text/css" href="${url}">`));
